bf29144cc442af4c780e67e3402418f8c8eba264
4amirbayat <4amirbayat@gmail.com>
2024-09-12 12:41:47 +0330

    Added routes for market page and market detail page

---
+++ src/routes/market/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type Props = {};
+const MarketListPage = (props: Props) => {
+  return <div>market list page</div>;
+};
+
+export default MarketListPage;
---
+++ src/routes/market/marketDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+type Props = {};
+const MarketDetailPage = (props: Props) => {
+  return <div>market detail</div>;
+};
+
+export default MarketDetailPage;
